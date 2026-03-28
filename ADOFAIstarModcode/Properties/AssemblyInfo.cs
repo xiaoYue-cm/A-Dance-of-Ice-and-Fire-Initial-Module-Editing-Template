026e93be11c53d1fe50a8a2ab813f7d2e2addceb
CerYin <cpoczq@outlook.com>
2026-03-28 09:58:09 +0800

    主要模板

---
+++ ADOFAIstarModcode/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// 这些引入库是用于程序属性的喵
+
+// 有关程序集的一般信息由以下喵
+// 控制。更改这些特性值可修改喵
+// 与程序集关联的信息。
+
+[assembly: AssemblyTitle("ADOFAImod")]
+// mod的一个说明性标题喵
+
+[assembly: AssemblyDescription("")]
+// mod的备注相当，当然我在Info.json里也有"Remarks"，估计没啥用，所以这个也可以不写喵
+
+[assembly: AssemblyConfiguration("")]
+// mod的调试用的，作用是生成配置文件，没什么用喵
+
+[assembly: AssemblyCompany("")]
+// mod工作室或者公司名称
+
+[assembly: AssemblyProduct("ADOFAImod")]
+// 用来标识mod的产品名称
+
+[assembly: AssemblyCopyright("Copyright ©  2026")] 
+// 版权信息喵
+
+[assembly: AssemblyTrademark("")]
+// 定制或者付费mod则可以使用商标信息喵
+
+[assembly: AssemblyCulture("")]
+// 文化信息，通常不需要设置，除非你的mod是针对特定语言或者地区的喵
+
+[assembly: ComVisible(false)]
+// 单独管理mod的类成员，一般没啥用，是作用于模组之间的通信，如果你需要和其他模组通信的话可以设置成true喵
+
+[assembly: Guid("f963bd49-5dce-4ac6-86c2-2e7c38185e1c")]
+// 模组的Guid，一般来说是随机生成的，模组加载器会根据这个Guid来识别不同的模组，所以每个模组都要有一个独特的Guid喵
+
+[assembly: AssemblyVersion("1.0.0.0")]
+// mod的版本信息，一般格式是主版本.次版本.生成号.修订号
+// 通常情况下要和Info.json里的版本号保持一致喵
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+// 指示win32文件版本，通常和AssemblyVersion保持一致喵
---
+++ ADOFAIstarModcode/main.cs
@@ -0,0 +1,63 @@
+﻿// 这是一个ADOFAI（A Dance of Fire and Ice）游戏的初始mod框架喵
+// 你可以在这个框架的基础上添加你想要的功能，比如修改游戏数据、添加新的关卡、改变游戏界面等等喵
+
+// 很重要的一点，如果你是第一次生成mod的dll文件，一般方法是这样的喵
+// 方法是点击Visual Studio上方的生成菜单，选择生成解决方案喵（快捷键是Ctrl+Shift+B）
+// 生成成功后会在项目文件夹下的bin/Debug或者bin/Release文件夹里生成一个dll文件，这个dll文件就是你的mod了喵
+// 最后你把这个mod的dll文件和Json文件打包成zip文件，拖进模组加载器里面即可使用喵
+
+// 如果你是第一次写mod，这玩意语言是C# 你需要学习C#语言来开发mod喵
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// visual studio最开始写入的东西，我不知道有没有用，反正不影响喵
+
+using UnityModManagerNet;
+// 引用UnityModManagerNet命名空间，提供了mod开发所需的类和方法，比如ModEntry、ModLogger等等喵
+// 我建议后面添加dll的时候放在lib文件夹里面，然后在项目属性里添加引用，这样比较清晰，也方便管理你的mod依赖的dll文件喵喵
+// 第一次接触visual studio的话，引用方法是在解决方案资源管理器里右键点击引用，选择添加引用，然后在浏览选项卡里找到你的dll文件，添加进去就好了喵
+
+namespace ADOFAImod //命名空间，建议起一个你的mod的名字，避免和其他mod吵架喵
+                    // 不过visual studio会自动生成一个命名空间，名字和你的项目名字一样喵
+{
+    public class main // 主类，visual studio会检测你是否重命名了main.cs，然后自动重命名成对应的名字
+                      // 对应的，如果修改了这个类名称，要在Info.json在修改参数"EntryMethod"
+                      // 一般来说固定格式是"模组ID.你修改的类名称.Load"
+
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        // 定义一个名为Logger的日志容器，类型是UnityModManager.ModEntry.ModLogger喵
+        // 在OnToggle方法中可以直接使用Logger.Log("我是梁志超他奶奶");喵
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        //mod加载时调用，返回true表示加载成功，返回false表示加载失败，失败会自动关闭mod喵
+        {
+            Logger = modEntry.Logger; //赋值日志记录器喵
+            modEntry.OnToggle = OnToggle; //开关状态改变调用喵
+            return true;//提示mod加载成功喵
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        // 定义一个名为OnToggle的方法，参数modEntry是当前mod的入口对象喵
+        // 在游戏的mod管理页面mod开关是控制这个参数value是开关状态，true表示开启，false表示关闭喵
+        {
+            if (value) //启动mod喵
+            {
+                Logger.Log("窝来啦喵"); 
+            }
+            else //关闭mod喵
+            {
+                Logger.Log("被关闭了呜"); 
+            }
+            return true; //返回true表示成功，返回false表示失败，失败则会自动关闭mod喵
+        }
+    }
+}
+
+
+// 很多废话，对吧喵
+// 你可以删除这些注释，或者保留它们以供参考，反正不影响代码的运行呢喵
